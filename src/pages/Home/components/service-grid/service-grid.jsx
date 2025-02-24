8a34d2c2b9e7555498cf2eda31a9396c492d2666
BekzodQodirovdev <oldakkaun4@gmail.com>
2025-02-24 21:46:54 +0500

    service grid

--- src/pages/Home/components/service-grid/service-grid.jsx
+++ src/pages/Home/components/service-grid/service-grid.jsx
@@ -6,26 +6,15 @@ import ArrowRight from "../../../../assets/svg/ArrowRight";
 
 const ServiceGrid = () => {
     return (
-        <div className="container mt-[40px] mb-[48px] md:mb-[32px]">
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-[1fr_1fr] gap-6">
-                <div className="relative lg:col-span-1">
-                    <img src={item1} alt="Living Room" className="w-full" />
-                    <div className="absolute top-[48px] left-[48px]">
-                        <h3 className="text-[34px] font-medium">Living Room</h3>
-                        <div className="w-[99px] cursor-pointer">
-                            <p className="flex gap-[4px]">
-                                Shop Now <ArrowRight />
-                            </p>
-                            <div className="w-full h-[2px] bg-black"></div>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="grid grid-rows-2 gap-[24px] lg:col-span-1">
-                    <div className="relative">
-                        <img src={item2} alt="Bedroom" className="w-full" />
-                        <div className="absolute bottom-[40px] left-[32px]">
-                            <h3 className="text-[34px] font-medium">Bedroom</h3>
+        <section className="mt-[40px] mb-[48px] md:mb-[32px]">
+            <div className="container">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-[1fr_1fr] gap-6">
+                    <div className="relative lg:col-span-1">
+                        <img src={item1} alt="Living Room" className="w-full" />
+                        <div className="absolute top-[48px] left-[48px]">
+                            <h3 className="text-[34px] font-medium">
+                                Living Room
+                            </h3>
                             <div className="w-[99px] cursor-pointer">
                                 <p className="flex gap-[4px]">
                                     Shop Now <ArrowRight />
@@ -35,21 +24,40 @@ const ServiceGrid = () => {
                         </div>
                     </div>
 
-                    <div className="relative">
-                        <img src={item3} alt="Kitchen" className="w-full" />
-                        <div className="absolute bottom-[40px] left-[32px]">
-                            <h3 className="text-[34px] font-medium">Kitchen</h3>
-                            <div className="w-[99px] cursor-pointer">
-                                <p className="flex gap-[4px]">
-                                    Shop Now <ArrowRight />
-                                </p>
-                                <div className="w-full h-[2px] bg-black"></div>
+                    <div className="grid grid-rows-2 gap-[24px] lg:col-span-1">
+                        <div className="relative">
+                            <img src={item2} alt="Bedroom" className="w-full" />
+                            <div className="absolute bottom-[40px] left-[32px]">
+                                <h3 className="text-[34px] font-medium">
+                                    Bedroom
+                                </h3>
+                                <div className="w-[99px] cursor-pointer">
+                                    <p className="flex gap-[4px]">
+                                        Shop Now <ArrowRight />
+                                    </p>
+                                    <div className="w-full h-[2px] bg-black"></div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="relative">
+                            <img src={item3} alt="Kitchen" className="w-full" />
+                            <div className="absolute bottom-[40px] left-[32px]">
+                                <h3 className="text-[34px] font-medium">
+                                    Kitchen
+                                </h3>
+                                <div className="w-[99px] cursor-pointer">
+                                    <p className="flex gap-[4px]">
+                                        Shop Now <ArrowRight />
+                                    </p>
+                                    <div className="w-full h-[2px] bg-black"></div>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </section>
     );
 };
 
