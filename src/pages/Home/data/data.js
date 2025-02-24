3656fa5dea3d60af7dbb9c5e7e18018e1909e8c9
SSR707 <samandarshavkatov07@gmail.com>
2025-02-24 17:54:14 +0500

    Product Card And Info Card End

---
+++ src/pages/Home/data/data.js
@@ -0,0 +1,72 @@
+import Product from "../../../assets/svg/product-img-1.svg";
+import CarIcon from "../../../assets/svg/carIcon.svg";
+import BlockIcon from "../../../assets/svg/blockInco.svg";
+import LockIcon from "../../../assets/svg/lockicon.svg";
+import PhoneIcon from "../../../assets/svg/phoneIcon.svg";
+
+export const ProductData = [
+  {
+    id: 1,
+    img: Product,
+    title: "Loveseat Sofa",
+    price: "199.000",
+    old_price: "$ 400.000",
+    skidka: "-50%",
+  },
+  {
+    id: 3,
+    img: Product,
+    title: "Loveseat Sofa",
+    price: "199.000",
+    old_price: "$ 400.000",
+    skidka: "-50%",
+  },
+  {
+    id: 4,
+    img: Product,
+    title: "Loveseat Sofa",
+    price: "199.000",
+    skidka: "-50%",
+  },
+  {
+    id: 5,
+    img: Product,
+    title: "Loveseat Sofa",
+    price: "199.000",
+  },
+  {
+    id: 6,
+    img: Product,
+    title: "Loveseat Sofa",
+    price: "199.000",
+  },
+  {
+    id: 7,
+    img: Product,
+    title: "Loveseat Sofa",
+    price: "199.000",
+  },
+];
+
+export const InfoData = [
+  {
+    icon: CarIcon,
+    title: "Free Shipping",
+    description: " Order above $200",
+  },
+  {
+    icon: BlockIcon,
+    title: "Money-back",
+    description: " 30 days guarantee",
+  },
+  {
+    icon: LockIcon,
+    title: "Secure Payments",
+    description: " Secured by Stripe",
+  },
+  {
+    icon: PhoneIcon,
+    title: "24/7 Support",
+    description: "Phone and Email support",
+  },
+];
