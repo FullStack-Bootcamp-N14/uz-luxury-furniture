edaaf572b1238b0ef501cc7e61c16ce7ee1b927e
muhiddinovismoil <muhiddinovismoil2@gmail.com>
2025-02-25 02:31:24 +0500

    Footer successfully finished with responsive

---
+++ src/assets/svg/footer/footer-social.jsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+const FooterSocial = (props) => {
+  return (
+    <svg width={23} height={22} viewBox="0 0 23 22" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <rect x={1.5} y={1} width={20} height={20} rx={4} stroke="#FEFEFE" strokeWidth={1.5}/>
+    <circle cx={17.5} cy={5} r={1} fill="#FEFEFE"/>
+    <circle cx={11.5} cy={11} r={5} stroke="#FEFEFE" strokeWidth={1.5}/>
+    </svg>
+  );
+};
+
+export default FooterSocial;
