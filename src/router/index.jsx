bc6386e968a4e7dcfd1978a46c52f76bfcc49dcc
WebCONuz <muxammadi0799@gmail.com>
2025-02-25 17:55:43 +0500

    Recived Ismoil branch

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,6 +1,8 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "../pages/Home/HomePage";
 import ShopPage from "../pages/Shop/ShopPage";
+import Footer from "../components/main/Footer";
+
 const Router = () => {
   return (
     <>
@@ -8,6 +10,7 @@ const Router = () => {
         <Route path="/" element={<HomePage />} />
         <Route path="/shop" element={<ShopPage />} />
       </Routes>
+      <Footer />
     </>
   );
 };
