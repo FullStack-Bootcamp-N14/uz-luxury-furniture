0a4a5c48df3ff6f7e9927d6badba5b61c947ff28
Abdurakxim254 <toxirmalk@gmail.com>
2025-02-26 21:56:32 +0500

    Project is ready

--- src/pages/Articles/article.jsx
+++ src/pages/Articles/article.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import { ArticleTitle } from './components/Articletitle/ArticleTitle'
 import { ArticleCard } from './components/ArticleCard/articleCard'
 import { ArticleData } from './components/Datas/articledatas'
+import Navigate from "@/assets/svg/navigate.svg";
+import "./article.css"
 
 export const Article = () => {
   return (
     <>
-      <section className=' w-[1440px] h-[657px]'>
-        <ArticleTitle />
-        <div className=' flex items-center justify-between pt-[40px] w-[1121px] h-[413px] gap-[25px] mr-[auto] ml-[auto]'>
+      <section className=' w-[1440px] h-[657px] article'>
+        <ArticleTitle title={"Articles"} info={"More Articles"} children={Navigate}>
+        <img src={Navigate} alt="Img" />
+        </ArticleTitle>
+        <div className=' flex items-center justify-between pt-[40px] w-[1121px] h-[413px] gap-[25px] mr-[auto] ml-[auto] articlecard'>
           {ArticleData.map((item) => (
             <ArticleCard key={item.id} title={item.title} subtitle={item.subtitle} img={item.img} navigate={item.navigate} />
           ))}
--- src/pages/Articles/components/Articletitle/ArticleTitle.jsx
+++ src/pages/Articles/components/Articletitle/ArticleTitle.jsx
@@ -1,14 +1,18 @@
-import Navigate from "@/assets/svg/navigate.svg";
-import Title from "../../../../components/ui/Title";
+import "./articletitle.css"
 
-export const ArticleTitle = () => {
+export const ArticleTitle = ({title,info,children}) => {
   return (
-    <section className="arrivals py-12">
-      <Title title="Articles" info="More Articles">
-        <img src={Navigate} alt="" className="w-[20px] h-[20px] " />
-      </Title>
+    <div className="article_title">
       <div className="container">
+        <div className="flex justify-between items-center ">
+          <h2 className=" font-medium text-[34px] leading-[112%] text-[#000] w-[125px] xl:text-[40px] xl:leading-[110%] title_text">
+            {title}
+          </h2>
+          <p className=" items-end gap-[4px] font-medium text-[16px] leading-[145%] border-b-1 flex">
+            {info} {children}
+          </p>
+        </div>
       </div>
-    </section>
+    </div>
   );
 };
