6c331a058570cd39b514cf2de6f7f35621b6c4d7
WebCONuz <muxammadi0799@gmail.com>
2025-02-28 17:33:26 +0500

    Recived mehriddin branch

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,5 +1,5 @@
 import Contact from "@/components/main/Contact";
-import HomeSlider from "./components/Home-slider/home-slider";
+import HomeSlider from "./components/home-slider/home-slider";
 import { Info } from "./components/info-grid/info";
 import { Arrivals } from "./components/product-slider/arrivals";
 import ServiceGrid from "./components/service-grid/service-grid";
