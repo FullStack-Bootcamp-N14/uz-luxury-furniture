dfa3d8e680894a0b773e6f6f1378287e1fb788d2
Abdurakxim254 <toxirmalk@gmail.com>
2025-02-28 18:40:33 +0500

    Commit

--- src/pages/Articles/article.jsx
+++ src/pages/Articles/article.jsx
@@ -7,13 +7,13 @@ import "./article.css"
 export const Article = () => {
   return (
     <>
-      <section className='py-[80px]'>
+      <section className='pb-4 lg:pb-20'>
         <div className='container'>
           <ArticleTitle title={"Articles"} info={"More Articles"} >
           </ArticleTitle>
-          <div className='flex flex-wrap items-center justify-between pt-[40px] gap-[25px]'>
+          <div className='flex flex-wrap lg:flex-nowrap pt-[40px] lg:gap-x-6'>
             {ArticleData.map((item) => (
-              < ArticleCard key={item.id} title={item.title} subtitle={item.subtitle} img={item.img} navigate={item.navigate} />
+              <ArticleCard key={item.id} title={item.title} subtitle={item.subtitle} img={item.img} navigate={item.navigate} />
             ))}
           </div>
         </div>
--- src/pages/Articles/components/ArticleCard/articleCard.jsx
+++ src/pages/Articles/components/ArticleCard/articleCard.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 
-export const ArticleCard = ({ title, img, subtitle,navigate }) => {
+export const ArticleCard = ({ title, img, subtitle, navigate }) => {
   return (
-    <div>
-      <div>
+    <div className='w-full lg:w-1/3 mb-6 lg:mb-0'>
         <div>
-          <img src={img} alt="Img" />
+          <img src={img} alt="Img" className='w-full' />
         </div>
         <div className=' pt-[24px]'>
           <div>
@@ -18,7 +17,6 @@ export const ArticleCard = ({ title, img, subtitle,navigate }) => {
             </div>
           </div>
         </div>
-      </div>
     </div>
   )
 }
