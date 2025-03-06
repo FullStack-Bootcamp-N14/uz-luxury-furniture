97679677c1698cf8c5b2f813fbb5e38f4d32474c
WebCONuz <muxammadi0799@gmail.com>
2025-03-06 18:01:58 +0500

    Completed wishlist functions

--- src/pages/Wishlist/Wishlist.jsx
+++ src/pages/Wishlist/Wishlist.jsx
@@ -1,5 +1,29 @@
+import { useSelector } from "react-redux";
+import { ProductCard } from "@/components/main/ProductCard";
+
 const Wishlist = () => {
-  return <div>Wishlist</div>;
+  const likeProducts = useSelector((state) => state.wishlist.likes);
+
+  return (
+    <main className="main py-10">
+      <div className="container">
+        <div className="text-center mb-10">
+          <h1 className="text-3xl font-semibold">Wish List</h1>
+        </div>
+        {likeProducts.length ? (
+          <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
+            {likeProducts.map((item) => (
+              <ProductCard key={item.id} productData={item} />
+            ))}
+          </div>
+        ) : (
+          <div className="flex items-center justify-center h-[60vh] rounded-2xl bg-gray-100">
+            <span className="text-lg font-medium uppercase">Empty Data</span>
+          </div>
+        )}
+      </div>
+    </main>
+  );
 };
 
 export default Wishlist;
